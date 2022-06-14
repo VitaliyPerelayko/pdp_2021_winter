77bd2a6555d0b253edab76871a7355cc785e567e
Vital Perelayko <vital_peralaika@senla.eu>
2022-06-14 12:03:42 +0300

    foreach iterate examples

--- lwc_superbadge/force-app/main/default/lwc/boatTile/boatTile.js
+++ lwc_superbadge/force-app/main/default/lwc/boatTile/boatTile.js
@@ -1,14 +1,26 @@
-import {api, LightningElement} from 'lwc';
+import {api, LightningElement, track} from 'lwc';
 
 const TILE_WRAPPER_SELECTED_CLASS = 'tile-wrapper selected';
 const TILE_WRAPPER_UNSELECTED_CLASS = 'tile-wrapper';
-
+const TEST_LIST = [
+    {key: 1, value: "one"},
+    {key: 2, value: "two"},
+    {key: 3, value: "three"},
+    {key: 4, value: "four"},
+    {key: 5, value: "five"},
+    {key: 6, value: "six"},
+    {key: 7, value: "seven"},
+    {key: 8, value: "eight"}
+]
 export default class BoatTile extends LightningElement {
 
     @api
     boat;
     @api
     selectedBoatId;
+
+    @track
+    testList = TEST_LIST;
 
     get backgroundStyle() {
         return 'background-image:url(' + this.boat.Picture__c + ')';
@@ -30,5 +42,15 @@ export default class BoatTile extends LightningElement {
             }
         });
         this.dispatchEvent(boatselect);
+    }
+
+    handleForCheckbox(event) {
+        const t = event.target;
+        console.log(t.id);
+        if (t.checked) {
+            t.parentElement.style.color = "red";
+        } else {
+            t.parentElement.style.color = "black";
+        }
     }
 }
